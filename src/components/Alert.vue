bf812d93878df9e48619834469baf8c9bdbd3ea2
yoichi kobayashi <info@tplh.net>
2020-04-12 23:45:46 +0900

    create Alert component.

---
+++ src/components/Alert.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'Alert'
+};
+</script>
+
+<template lang="pug">
+.alert
+  |navigator.mediaDevices is disabled.
+</template>
+
+<style lang="scss" scoped>
+.alert {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+</style>
