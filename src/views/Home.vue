0c39b179d8f5b831ee586b7d81bfb41fdabd9c44
yoichi kobayashi <info@tplh.net>
2020-04-27 16:12:39 +0900

    adjusted the layout of the home view.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,45 @@
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+    stylesWrap() {
+      const { resolution } = this.$store.state;
+      return {
+        width: `${resolution.x}px`,
+        height: `${resolution.y}px`
+      };
+    }
+  }
 };
 </script>
 
 <template lang="pug">
+.wrap(
+  :style = 'stylesWrap'
+  )
+  div
+    h1
+      |sketch-webcam
+    p
+      |Interactive demos with webcam,
+      br
+      |tensorflow.js models, three.js and Vue-CLI.
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  line-height: 1;
+  margin: 0 0 1em;
+  @include fontSizeAll(28, 28, 21);
+  letter-spacing: 0.3em;
+}
+p {
+  margin: 0;
+}
+</style>
