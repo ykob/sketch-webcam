9d82b9484336a6c9d8c8405bb9e88b5a17363ec5
yoichi kobayashi <info@tplh.net>
2020-05-06 19:39:44 +0900

    made the button component.

---
+++ src/components/common/Button.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Button'
+};
+</script>
+
+<template lang="pug">
+button
+  slot
+</template>
+
+<style lang="scss" scoped>
+button {
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+}
+</style>
