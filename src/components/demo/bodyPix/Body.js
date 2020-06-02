e6c159e1378a1dfcf8e68bc5d0fa1a352330ae02
yoichi kobayashi <info@tplh.net>
2020-06-02 23:58:22 +0900

    clean up.

--- src/components/demo/bodyPix/Body.js
+++ src/components/demo/bodyPix/Body.js
@@ -58,8 +58,7 @@ export default class Body extends Mesh {
     texture.value.needsUpdate = true;
   }
   resize() {
-    const { camera } = store.state;
-
+    const { camera, webcam } = store.state;
     const height = Math.abs(
       (camera.position.z - this.position.z) *
         Math.tan(MathEx.radians(camera.fov) / 2) *
@@ -69,11 +68,9 @@ export default class Body extends Mesh {
 
     this.size.set(width, height, 1);
     this.scale.copy(this.size);
-
-    const { resolution } = store.state.webcam;
     this.material.uniforms.imgRatio.value.set(
-      Math.min(1, ((this.size.x / this.size.y) * resolution.y) / resolution.x),
-      Math.min(1, ((this.size.y / this.size.x) * resolution.x) / resolution.y)
+      Math.min(1, ((this.size.x / this.size.y) * webcam.resolution.y) / webcam.resolution.x),
+      Math.min(1, ((this.size.y / this.size.x) * webcam.resolution.x) / webcam.resolution.y)
     );
   }
 }
