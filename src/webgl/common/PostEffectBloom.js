6fdd722a0b5aa83d6e63148fa93ae87646d9eead
yoichi kobayashi <info@tplh.net>
2020-06-04 22:54:53 +0900

    added the common post effect components.

---
+++ src/webgl/common/PostEffectBloom.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+
+import store from '@/store';
+
+import vs from './glsl/PostEffect.vs';
+import fs from './glsl/PostEffectBloom.fs';
+
+export default class PostEffectBloom extends THREE.Mesh {
+  constructor() {
+    // Define Geometry
+    const geometry = new THREE.PlaneBufferGeometry(2, 2);
+
+    // Define Material
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        time: {
+          value: 0
+        },
+        resolution: {
+          value: store.state.resolution
+        },
+        texture1: {
+          value: null
+        },
+        texture2: {
+          value: null
+        }
+      },
+      vertexShader: vs,
+      fragmentShader: fs
+    });
+
+    // Create Object3D
+    super(geometry, material);
+    this.name = 'PostEffectBloom';
+    this.isShaking = false;
+    this.isFadeOut = false;
+  }
+  start(texture1, texture2) {
+    this.material.uniforms.texture1.value = texture1;
+    this.material.uniforms.texture2.value = texture2;
+  }
+}
---
+++ src/webgl/common/PostEffectBlur.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+
+import store from '@/store';
+
+import vs from './glsl/PostEffect.vs';
+import fs from './glsl/PostEffectBlur.fs';
+
+export default class PostEffectBlur extends THREE.Mesh {
+  constructor() {
+    // Define Geometry
+    const geometry = new THREE.PlaneBufferGeometry(2, 2);
+
+    // Define Material
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        resolution: {
+          value: new THREE.Vector2()
+        },
+        direction: {
+          value: new THREE.Vector2()
+        },
+        texture: {
+          value: null
+        }
+      },
+      vertexShader: vs,
+      fragmentShader: fs
+    });
+
+    // Create Object3D
+    super(geometry, material);
+    this.name = 'PostEffectBlur';
+  }
+  start(texture, x, y) {
+    this.material.uniforms.texture.value = texture;
+    this.material.uniforms.direction.value.set(x, y);
+  }
+  resize() {
+    const { resolution } = store.state;
+    this.material.uniforms.resolution.value.set(
+      resolution.x / 3,
+      resolution.y / 3
+    );
+  }
+}
