06a4b572da181dd97b26025fb60e18c64fceca7d
yoichi kobayashi <info@tplh.net>
2020-06-06 22:57:02 +0900

    added the blob objects on the bodypix demo.

--- src/views/demo/BodyPix.vue
+++ src/views/demo/BodyPix.vue
@@ -5,9 +5,15 @@ import store from '@/store';
 
 import DemoConsole from '@/components/common/DemoConsole';
 import PostEffectBlur from '@/webgl/common/PostEffectBlur';
+import Blob from '@/webgl/demo/bodyPix/Blob';
 import Body from '@/webgl/demo/bodyPix/Body';
 import Video from '@/webgl/demo/bodyPix/Video';
 import VideoBack from '@/webgl/demo/bodyPix/VideoBack';
+
+const arr = [1, 2, 3, 4, 5, 6];
+const blobs = arr.map(() => {
+  return new Blob();
+});
 
 export default {
   name: 'BodyPix',
@@ -18,6 +24,7 @@ export default {
     net: null,
     timeSegment: 0,
     scenePE: new Scene(),
+    blobs: blobs,
     body: new Body(),
     video: new Video(),
     videoBack: new VideoBack(),
@@ -32,7 +39,12 @@ export default {
     this.video.start(this.renderTarget1.texture);
     this.videoBack.start(this.renderTarget1.texture);
     this.postEffectBlurX.start(this.renderTarget1.texture, 1, 0);
+    this.postEffectBlurX.setScale(0.5, 0.5);
     this.postEffectBlurY.start(this.renderTarget2.texture, 0, 1);
+    this.postEffectBlurY.setScale(0.5, 0.5);
+    this.blobs.forEach(blob => {
+      state.scene.add(blob);
+    });
     state.scene.add(this.video);
     state.scene.add(this.videoBack);
 
@@ -53,6 +65,9 @@ export default {
     const { state, commit } = store;
     state.scene.remove(this.video);
     state.scene.remove(this.videoBack);
+    this.blobs.forEach(blob => {
+      state.scene.remove(blob);
+    });
     commit('destroyUpdate');
     commit('destroyResize');
   },
@@ -61,15 +76,19 @@ export default {
       const { state } = store;
 
       this.timeSegment += time;
-      if (this.timeSegment >= 1 / 30) {
+      if (this.timeSegment >= 1 / 60) {
         const segmentation = await this.net.segmentPerson(state.webcam.video, {
           flipHorizontal: true,
-          internalResolution: 'medium',
-          segmentationThreshold: 0.7
+          internalResolution: 'low',
+          segmentationThreshold: 0.8
         });
         this.body.updateSegmentation(segmentation);
         this.timeSegment = 0;
       }
+
+      this.blobs.forEach(blob => {
+        blob.update(time);
+      });
 
       // // Render the post effect.
       this.scenePE.add(this.body);
---
+++ src/webgl/demo/bodyPix/Blob.js
@@ -0,0 +1,40 @@
+import { Mesh, BoxBufferGeometry, RawShaderMaterial } from 'three';
+
+import vs from './glsl/Blob.vs';
+import fs from './glsl/Blob.fs';
+
+export default class Blob extends Mesh {
+  constructor() {
+    // Define Geometry
+    const geometry = new BoxBufferGeometry(1.5, 1.5, 1.5);
+
+    // Define Material
+    const material = new RawShaderMaterial({
+      uniforms: {
+        time: {
+          value: 0
+        }
+      },
+      vertexShader: vs,
+      fragmentShader: fs,
+      flatShading: true
+    });
+    super(geometry, material);
+    this.random1 = Math.random();
+    this.random2 = Math.random();
+  }
+  update(t) {
+    const { time } = this.material.uniforms;
+    time.value += t;
+    this.position.set(
+      Math.cos(time.value * 1.5 + this.random1 * 20) * 12,
+      Math.sin(time.value / 2 + this.random1 * 1000) * 5,
+      Math.sin(time.value * 1.5 + this.random1 * 20) * 12
+    );
+    this.rotation.set(
+      time.value * 2 + this.random2 * 20,
+      time.value * 2 + this.random2 * 20,
+      time.value * 2 + this.random2 * 20
+    );
+  }
+}
