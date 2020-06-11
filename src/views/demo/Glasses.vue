cd2f3fae6db72b680e25483338b5ac2d3de8507d
yoichi kobayashi <info@tplh.net>
2020-06-11 22:53:42 +0900

    added the process to the load the Glasses object.

--- src/views/demo/Glasses.vue
+++ src/views/demo/Glasses.vue
@@ -3,6 +3,7 @@ import * as facemesh from '@tensorflow-models/facemesh';
 import store from '@/store';
 
 import DemoConsole from '@/components/common/DemoConsole';
+import PromiseOBJLoader from '@/webgl/common/PromiseOBJLoader';
 import Video from '@/webgl/demo/glasses/Video';
 
 export default {
@@ -12,19 +13,28 @@ export default {
   },
   data: () => ({
     video: new Video(),
+    glasses: null,
     model: null,
     timeSegment: 0
   }),
   async created() {
     const { state, commit, dispatch } = store;
 
-    state.scene.add(this.video);
-
-    dispatch('webcam/init').then(async () => {
-      this.model = await facemesh.load({
+    Promise.all([
+      dispatch('webcam/init'),
+      facemesh.load({
         maxFaces: 1,
         iouThreshold: 0.1
-      });
+      }),
+      PromiseOBJLoader(`${process.env.BASE_URL}obj/Glasses_01.obj`)
+    ]).then(async response => {
+      if (this._isDestroyed !== false) return;
+
+      this.glasses = response[2];
+      this.model = response[1];
+
+      state.scene.add(this.glasses);
+      state.scene.add(this.video);
 
       commit('setUpdate', this.update);
       commit('setResize', this.resize);
@@ -33,19 +43,20 @@ export default {
   },
   destroyed() {
     const { state, commit } = store;
+
+    state.scene.remove(this.glasses);
     state.scene.remove(this.video);
     commit('destroyUpdate');
     commit('destroyResize');
   },
   methods: {
     async update(time) {
-      const { state } = store;
+      // const { state } = store;
 
       this.timeSegment += time;
       if (this.timeSegment >= 1 / 60) {
-        const predictions = await this.model.estimateFaces(state.webcam.video);
+        // const predictions = await this.model.estimateFaces(state.webcam.video);
         this.timeSegment = 0;
-        console.log(predictions);
       }
     },
     resize() {
