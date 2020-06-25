7982f89ee6dd142cd7ae0fbaad5159d59c9b4916
yoichi kobayashi <info@tplh.net>
2020-06-25 18:51:25 +0900

    added a signature and share links to the home header component.

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -1,6 +1,13 @@
 <script>
+import IconGitHub from '@/components/icon/IconGitHub';
+import IconTwitter from '@/components/icon/IconTwitter';
+
 export default {
-  name: 'HomeHeader'
+  name: 'HomeHeader',
+  components: {
+    IconGitHub,
+    IconTwitter
+  }
 };
 </script>
 
@@ -9,13 +16,33 @@ transition(
   appear
   )
   .home-header
-    div
-      h1
+    .home-header__in
+      h1.home-header__title
         |sketch-webcam
-      p
+      p.home-header__description
         |Interactive demos with webcam,
         br
         |tensorflow.js models, three.js and Vue-CLI.
+      .home-header__options
+        p.home-header__signature
+          |created by 
+          a(
+            href = 'https://www.tplh.net/'
+            target = '_blank'
+            )
+            |yoichi kobayashi.
+        .home-header__links
+          a.home-header__link(
+              href = 'https://github.com/ykob/sketch-webcam'
+              target = '_blank'
+              )
+              IconGitHub
+          a.home-header__link(
+            href = 'https://twitter.com/ykob0123'
+            target = '_blank'
+            )
+            IconTwitter
+      
 </template>
 
 <style lang="scss" scoped>
@@ -46,14 +73,51 @@ transition(
     opacity: 1;
     transition-duration: 1s;
   }
-}
-h1 {
-  line-height: 1;
-  margin: 0 0 0.3em;
-  @include fontSizeAll(42, 42, 28);
-  letter-spacing: 0.12em;
-}
-p {
-  margin: 0;
+  &__title {
+    line-height: 1;
+    margin-top: 0;
+    @include fontSizeAll(42, 42, 28);
+    letter-spacing: 0.12em;
+    @include l-more-than-mobile {
+      margin-bottom: 16px;
+    }
+    @include l-mobile {
+      margin-bottom: 8px;
+    }
+  }
+  &__description {
+    margin-top: 0;
+    @include l-more-than-mobile {
+      margin-bottom: 24px;
+    }
+    @include l-mobile {
+      margin-bottom: 16px;
+    }
+  }
+  &__options {
+    @include l-more-than-mobile {
+      display: flex;
+      align-items: center;
+    }
+  }
+  &__signature {
+    margin: 0;
+    @include fontSizeAll(11, 11, 9);
+    @include l-more-than-mobile {
+      margin-right: 32px;
+    }
+    @include l-mobile {
+      margin-bottom: 8px;
+    }
+  }
+  &__links {
+    display: flex;
+  }
+  &__link {
+    width: 24px;
+    height: 24px;
+    display: block;
+    margin-right: 12px;
+  }
 }
 </style>
