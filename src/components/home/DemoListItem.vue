8fe87f972f89e7f5001a1dd6377ac7c8fb14ad2f
yoichi kobayashi <koba@tsumikiinc.com>
2020-06-26 17:42:42 +0900

    added the transition effect to the demo list item component.

--- src/components/home/DemoListItem.vue
+++ src/components/home/DemoListItem.vue
@@ -19,6 +19,9 @@ export default {
     }
   },
   computed: {
+    classnames() {
+      return [`demo-list-item--${this.index + 1}`];
+    },
     number() {
       return `#${zeroPadding(this.index, 3)}`;
     }
@@ -34,14 +37,18 @@ export default {
 </script>
 
 <template lang="pug">
-a.demo-list-item(
-  :href = 'demo.path'
-  @click = 'click'
-)
-  .demo-list-item__num
-    |{{number}}
-  .demo-list-item__label
-    |{{ demo.name }}
+transition(
+  appear
+  )
+  a.demo-list-item(
+    :href = 'demo.path'
+    :class = 'classnames'
+    @click = 'click'
+    )
+    .demo-list-item__num
+      |{{number}}
+    .demo-list-item__label
+      |{{ demo.name }}
 </template>
 
 <style lang="scss" scoped>
@@ -72,6 +79,32 @@ a.demo-list-item(
   &__label {
     @include fontSizeAll(28, 28, 21);
     letter-spacing: 0.12em;
+  }
+
+  &.v-enter {
+    opacity: 0;
+    transform: translate3d(0, 10px, 0);
+  }
+  &.v-enter-to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    transition-duration: 1s;
+    transition-property: opacity, transform;
+  }
+  &.v-leave-to {
+    opacity: 0;
+    transform: translate3d(0, -10px, 0);
+    transition-duration: 1s;
+    transition-property: opacity, transform;
+  }
+
+  @for $i from 1 through 100 {
+    &:nth-of-type(#{$i}) {
+      &.v-enter-to,
+      &.v-leave-to {
+        transition-delay: (($i - 1) * 0.06) * 1s;
+      }
+    }
   }
 }
 </style>
