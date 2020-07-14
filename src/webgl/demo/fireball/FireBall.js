87096a771a235ace8658fd35cc1ef6b97f60c286
yoichi kobayashi <info@tplh.net>
2020-07-14 23:52:11 +0900

    use the points score to move the fire ball.

--- src/webgl/demo/fireball/FireBall.js
+++ src/webgl/demo/fireball/FireBall.js
@@ -8,6 +8,8 @@ export default class FireBall extends Group {
   constructor() {
     super();
 
+    this.a1 = 0;
+    this.a2 = 0;
     this.p1 = new Vector3();
     this.p2 = new Vector3();
     this.p3 = new Vector3();
@@ -18,7 +20,9 @@ export default class FireBall extends Group {
 
     this.add(this.core);
   }
-  update({ position }) {
+  update({ position, opacity }) {
+    this.a1 = opacity.getX(9);
+    this.a2 = opacity.getX(10);
     this.p1.set(position.getX(7), position.getY(7), 0);
     this.p2.set(position.getX(8), position.getY(8), 0);
     this.p3.set(position.getX(9), position.getY(9), 0);
@@ -35,11 +39,13 @@ export default class FireBall extends Group {
         .sub(this.p2)
         .divideScalar(3)
     );
-    const l = this.p5.clone().lerp(this.p6, 0.5);
-    const a = l
-      .clone()
-      .sub(this.position)
-      .divideScalar(20);
-    this.position.add(a);
+    if (this.a1 * this.a2 > 0) {
+      const l = this.p5.clone().lerp(this.p6, 0.5);
+      const a = l
+        .clone()
+        .sub(this.position)
+        .divideScalar(20);
+      this.position.add(a);
+    }
   }
 }
