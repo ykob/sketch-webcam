5cf3e11770c1190bf27d9bcbd01d09906f48fe81
yoichi kobayashi <info@tplh.net>
2020-07-25 01:42:49 +0900

    updated the progress marker.

--- src/components/demo/DemoOutline.vue
+++ src/components/demo/DemoOutline.vue
@@ -1,11 +1,6 @@
 <script>
-import Processing from '@/components/common/Processing';
-
 export default {
   name: 'DemoOutline',
-  components: {
-    Processing
-  },
   props: {
     title: {
       type: String,
@@ -28,7 +23,7 @@ transition(
       |{{ title }}
     p.demo-outline__description
       |{{ description }}
-    Processing
+    .demo-outline__processing-marker
 </template>
 
 <style lang="scss" scoped>
@@ -62,10 +57,39 @@ transition(
     margin: 0;
     text-align: center;
     @include l-more-than-mobile {
-      margin-bottom: 40px;
+      margin-bottom: 32px;
     }
     @include l-mobile {
-      margin-bottom: 24px;
+      margin-bottom: 16px;
+    }
+  }
+  @keyframes rotateLoop {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(720deg);
+    }
+  }
+  &__processing-marker {
+    @include l-more-than-mobile {
+      width: 32px;
+      height: 32px;
+    }
+    @include l-mobile {
+      width: 24px;
+      height: 24px;
+    }
+    &:before {
+      width: 100%;
+      height: 100%;
+      content: '';
+      display: block;
+      border: 2px solid $color-key;
+      animation-name: rotateLoop;
+      animation-duration: 3s;
+      animation-timing-function: $easeInOutSine;
+      animation-iteration-count: infinite;
     }
   }
   &.v-enter {
@@ -74,11 +98,15 @@ transition(
   &.v-enter & {
     &__title {
       opacity: 0;
-      transform: translate3d(0, 10px, 0);
+      transform: translate3d(0, 16px, 0);
     }
     &__description {
       opacity: 0;
-      transform: translate3d(0, 10px, 0);
+      transform: translate3d(0, 16px, 0);
+    }
+    &__processing-marker {
+      opacity: 0;
+      transform: scale(0.5);
     }
   }
   &.v-enter-to {
@@ -91,6 +119,7 @@ transition(
       opacity: 1;
       transform: translate3d(0, 0, 0);
       transition-duration: 1s;
+      transition-timing-function: $easeOutCubic;
       transition-property: opacity, transform;
     }
     &__description {
@@ -98,6 +127,15 @@ transition(
       transform: translate3d(0, 0, 0);
       transition-duration: 1s;
       transition-delay: 0.1s;
+      transition-timing-function: $easeOutCubic;
+      transition-property: opacity, transform;
+    }
+    &__processing-marker {
+      opacity: 1;
+      transform: scale(1);
+      transition-duration: 1s;
+      transition-delay: 0.2s;
+      transition-timing-function: $easeOutCubic;
       transition-property: opacity, transform;
     }
   }
@@ -109,15 +147,25 @@ transition(
   &.v-leave-to & {
     &__title {
       opacity: 0;
-      transform: translate3d(0, -10px, 0);
+      transform: translate3d(0, -16px, 0);
       transition-duration: 1s;
+      transition-timing-function: $easeOutCubic;
       transition-property: opacity, transform;
     }
     &__description {
       opacity: 0;
-      transform: translate3d(0, -10px, 0);
+      transform: translate3d(0, -16px, 0);
       transition-duration: 1s;
-      transition-delay: 0.1s;
+      transition-delay: 0.06s;
+      transition-timing-function: $easeOutCubic;
+      transition-property: opacity, transform;
+    }
+    &__processing-marker {
+      opacity: 0;
+      transform: scale(0.5);
+      transition-duration: 1s;
+      transition-delay: 0.12s;
+      transition-timing-function: $easeOutCubic;
       transition-property: opacity, transform;
     }
   }
