ce33062d269ec6ec30eb32235af3a1733dc1ff0c
yoichi kobayashi <info@tplh.net>
2020-08-24 23:41:21 +0900

    added crossfade effect of the fireball flare.

--- src/webgl/demo/fireball/glsl/FireBallFlare.fs
+++ src/webgl/demo/fireball/glsl/FireBallFlare.fs
@@ -2,6 +2,8 @@ precision highp float;
 
 uniform float time;
 uniform sampler2D texture;
+uniform float alphaShow;
+uniform float alphaHide;
 
 varying vec3 vPosition;
 varying vec2 vUv;
@@ -9,10 +11,12 @@ varying vec2 vUv;
 #pragma glslify: convertHsvToRgb = require(glsl-util/convertHsvToRgb);
 
 void main() {
+  float alpha = alphaShow * (1.0 - alphaHide);
+
   // Define Colors
   float texR = 1.0 - texture2D(texture, vUv + vec2(time * 0.2, 0.0)).g;
   float texG = 1.0 - texture2D(texture, vUv - vec2(time * 0.2, 0.0)).g;
-  float strength = smoothstep(0.7, 1.0, sin(radians((texR * 0.5 + texG * 0.5) * 360.0)) * 0.5 + 0.5);
+  float strength = smoothstep(0.7, 1.0, sin(radians((texR * 0.5 + texG * 0.5) * 360.0)) * 0.5 + 0.5 - (1.0 - alpha) * 0.3);
   strength = pow(strength, 2.0);
   vec3 hsv = vec3(
     strength * 0.14,
